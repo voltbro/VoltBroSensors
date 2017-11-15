ccdd472bd831d805bafb5846dcd8fea6006da963
Nikolay Slabuha <cola@cactus.ru>
2017-11-15 19:10:14 +0300

    BMP 180 first attemp

--- VoltBroSensors.h
+++ VoltBroSensors.h
@@ -16,6 +16,8 @@ class VoltBroSensors {
 		static void I2C_ReadBytes(uint8_t dev_addr, uint8_t register_addr, uint8_t num, uint8_t *buffer);
 		static void I2C_writeRegister8(uint8_t dev_addr, uint8_t register_addr, uint8_t value);
 		static uint8_t I2C_fastRegister8(uint8_t dev_addr, uint8_t register_addr);
+		static int32_t I2C_getRegister32(uint8_t dev_addr, uint8_t register_addr, uint8_t num, boolean unsign=true);//	Чтение данных от сенсора, NumByte - кол-во байт, Reg - адрес регистра, Uns - читать с учетом знака
+		//
 };
 
 #endif
